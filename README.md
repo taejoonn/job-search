9050cb72343eaad0ecc81eab0e21dc57f8ca7b4a
Alex <alexander.doo@stonybrook.edu>
2020-09-21 16:13:43 -0400

    GlassDoor added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 job-search
+
+
+A web app which scraps the results of several job board sites and brings them together in one web page.
+
+Location specification - [City, State(Abbreviated)]
+e.g. New York, NY
+
+---Installation---
+pip install
+python manage.py runserver
--- job_search/views/results.py
+++ job_search/views/results.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 from bs4 import BeautifulSoup
+from . import states_abb
 import requests
 import urllib.request
 
@@ -9,18 +10,23 @@ def results(request):
     sites = request.POST.getlist('sites')
     location = request.POST.get('location')
     role = request.POST.get('position')
-    index = int(request.POST.get("Indeed_page"))
-    print(sites)
+    ind_index = int(request.POST.get("Indeed_page"))
+    gd_index = int(request.POST.get("GlassDoor_page"))
 
     for x in sites:
-        if (x == "indeed"):
-            indeed(role, location, index)
+        if x == "indeed":
+            indeed(role, location, ind_index)
+        elif x == "glassdoor":
+            glassDoor(role, location, gd_index)
+        elif x == "linkedin":
+            print("linkedin")
 
     context = {
         "sites": sites,
-        "Indeed_page": index,
+        "Indeed_page": ind_index,
         "position": role,
-        "location": location
+        "location": location,
+        "GlassDoor_page": gd_index
     }
     
     return render(request, 'results.html', context=context)
@@ -36,6 +42,7 @@ def indeed(position, location, index):
     if index > 0:
         page = "&start=" + str(index*10)
 
+    # obtain ResultSet for job listings
     url = 'https://www.indeed.com/jobs?q=' + position + '&l=' + location + page
     print(url)
     response = requests.get(url)
@@ -59,3 +66,49 @@ def indeed(position, location, index):
     with open("job_search/templates/indeed.html", "w", encoding="utf-8") as file:
         file.write(result)
     file.close()
+
+
+def glassDoor(position, location, index):
+    result = ""
+
+    # create url string
+    url = "https://www.glassdoor.com/Job/"
+    position = position.replace(" ", "-")
+    location = location.split(", ")[1]
+    location = states_abb.states[location]
+    location = location.replace(" ", "-")
+    location = location.lower()
+    url += location + "-" + position + "-jobs-SRCH_IL.0,8_IC1132348_KO9,26.htm"
+    # determine page number
+    page = ".htm"
+    if index > 0:
+        page = "_IP" + str(index+1) + ".htm"
+    url = url.replace(".htm", page)
+    
+    # obtain ResultSet for job listings
+    headers = requests.utils.default_headers()
+    headers.update({
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+    })
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    allResults = soup.find_all(class_="jl react-job-listing gdGrid")
+
+    # convert ResultSet to string
+    for x in allResults:
+        result += str(x)
+    # split string at 'href'
+    resultList = result.split("href=")
+    result = ""
+    # loop and insert 'indeed.com' at href tags
+    for x, y in enumerate(resultList):
+        if x == 0:
+            result += y
+            continue
+        result += "target=" + y[0] + "_blank" + y[0] +" href=" + y[0] + "https://www.glassdoor.com" + y[1:]
+    
+    # write ResultSet to html file which will be imported in frontend
+    with open("job_search/templates/glassdoor.html", "w", encoding="utf-8") as file:
+        file.write(result)
+    file.close()
+    
